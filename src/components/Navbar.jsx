57d3548365ddcfecd2f77bab95036fa21fecbd9a
Ahmed Abdelwahed <ahmed.abdelwahed.dev@gmail.com>
2026-08-12 15:12:40 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import dayjs from "dayjs"
+import { devName, navLinks, navIcons } from "../constatnts/index.js"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="Logo" />
+                <p className="font-bold">{devName}'s Portfolio</p>
+
+                <ul>
+                    {navLinks.map(({ id, name }) => (
+                        <li key={id}>
+                            <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img src={img} alt={`icon-${id}`} />
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
